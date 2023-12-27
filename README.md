493c5034d4abf799f2c6b237d098f22c5e9d4bde
Sgobbi Federico <federico@sgobbi.it>
2023-12-27 23:54:36 +0100

    Init repo

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # esm-usb-rs485-adapter
-Nodejs library for USB to RS485 adapter 
---
+++ src/index.js
@@ -0,0 +1,12 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                                 *
+ *    Copyright (c) 2023 Sgobbi Federico                                                           *
+ *    All rights reserved.                                                                         *
+ *                                                                                                 *
+ *    This file is licensed under the MIT License.                                                 *
+ *    License text available at https://opensource.org/licenses/MIT                                *
+ *                                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// > > > > > > > > > > > > > > > > > > > > > > > The code
+console.log('Hello World!');
